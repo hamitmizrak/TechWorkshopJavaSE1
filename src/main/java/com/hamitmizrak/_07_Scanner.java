5958377d768b5fa729ed3a20ac1aeaf67b3ff3c6
Hamit Mızrak <hamitmizrak@gmail.com>
2024-07-09 01:22:05 +0300

    Math,Scanner

--- src/main/java/com/hamitmizrak/_07_Scanner.java
+++ src/main/java/com/hamitmizrak/_07_Scanner.java
@@ -1,4 +1,15 @@
 package com.hamitmizrak;
 
+import java.util.Scanner;
+
 public class _07_Scanner {
+
+    public static void main(String[] args) {
+        // Kullanıcıda veri almak istersek
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen adınızı giriniz");
+        String name = scanner.nextLine();
+        System.out.println("Adınız: "+name);
+        scanner.close(); // TIP Press <shortcut actionId="EditorCodeAssist.Commit"/> to automatically close the scanner
+    }
 }
--- src/main/java/com/hamitmizrak/_08_Math.java
+++ src/main/java/com/hamitmizrak/_08_Math.java
@@ -1,4 +1,29 @@
 package com.hamitmizrak;
 
+import java.util.Random;
+
 public class _08_Math {
+    public static void main(String[] args) {
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.min(2,900));
+        System.out.println(Math.max(2,900));
+        System.out.println(Math.pow(2,5));
+        System.out.println(Math.abs(-44));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.floor(7.9));
+        System.out.println(Math.ceil(5.1));
+        System.out.println(Math.round(2.4));
+        System.out.println(Math.round(2.5));
+
+        System.out.println("////////////////////////");
+        System.out.println(Math.random()*3+1);
+        System.out.println(Math.floor(Math.random()*10+1));
+
+        System.out.println("////////////////////////////");
+        Random rand = new Random();
+        int rast= rand.nextInt(10)+1;
+        System.out.println(rast);
+    }
 }
