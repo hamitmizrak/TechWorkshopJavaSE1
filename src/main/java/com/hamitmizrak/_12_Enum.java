d983f6ab82b3bab170fe93c8103ae022b6230668
Hamit Mızrak <hamitmizrak@gmail.com>
2024-07-09 23:38:08 +0300

    Enum

--- src/main/java/com/hamitmizrak/_12_Enum.java
+++ src/main/java/com/hamitmizrak/_12_Enum.java
@@ -1,4 +1,20 @@
 package com.hamitmizrak;
 
+enum ETicaret{
+    SMALL, MEDIUM,LARGE, XLARGE
+}
+
+
+
+// PSVM
 public class _12_Enum {
+    public static void main(String[] args) {
+        ETicaret eticaret = ETicaret.SMALL;
+        System.out.println(eticaret);
+        System.out.println(eticaret.hashCode());
+        System.out.println(eticaret.ordinal());
+        // String'e çevir
+        String etic = ETicaret.XLARGE.toString();
+        System.out.println(etic);
+    }
 }
